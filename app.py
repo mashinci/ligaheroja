cdc0c1390fef7e01237be57093b13f50f4a3bdeb
djolearn <80420514+djolearn@users.noreply.github.com>
2023-08-26 17:41:02 +0200

    Added new route

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+@app.route("/champSelect")
+def champ_select():
+    return render_template('champSelect.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+    
