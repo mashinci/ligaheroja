856ce9b8066df6ef10f7b5d1ce0819e44b4ee618
djolearn <80420514+djolearn@users.noreply.github.com>
2023-08-28 10:50:34 +0200

    /

--- static/js/champSelect.js
+++ static/js/champSelect.js
@@ -1,21 +1,33 @@
 "use strict";
-const drafted_player_heroes = document.querySelector(".drafted-player-heroes");
 const drafted_player_container = document.querySelectorAll(
   ".drafted-player-container"
 );
+const drafted_player_heroes_form = document.querySelector(
+  ".drafted-player-heroes"
+);
+// One Player image variable
+const one_player_img = document.querySelector(".drafted-player-img");
+// Team points variables
+const blue_team_points = document.querySelector("#blue-team-points");
+const red_team_points = document.querySelector("#red-team-points");
+let brojPoena = 15;
+
+// On click player to show heroes
 const radio_twisted_fate = document.querySelector("#twistedFate");
 drafted_player_container.forEach((container) => {
   const player_img = container.querySelector(".drafted-player-img");
   player_img.addEventListener("click", function () {
-    drafted_player_heroes.style.opacity = 1;
+    drafted_player_heroes_form.style.opacity = 1;
   });
 });
-let brojPoena = 15;
+
+// Points lower, Form disappear when Twisted Fate Checked
 console.log(brojPoena);
 radio_twisted_fate.addEventListener("change", function () {
   if (radio_twisted_fate.checked) {
     brojPoena -= 5;
-    drafted_player_heroes.style.opacity = 0;
+    blue_team_points.textContent = brojPoena;
+    drafted_player_heroes_form.style.opacity = 0;
     console.log("Posle izbora");
     console.log(brojPoena);
   }
