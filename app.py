637f8d4479f3f65fe268cad90a32b4900bd7b5ad
koleDE <nikola.petrovic@primeholding.com>
2023-09-07 15:27:07 +0200

    dropped extra code

--- app.py
+++ app.py
@@ -18,19 +18,10 @@ def draft_submit():
     blue_team = draft_data.get('blue_team', [])
     red_team = draft_data.get('red_team', [])
     
-    teams_response_data = {
-        'message': 'Data received successfully',
-        'blue_team': blue_team,
-        'red_team': red_team
-    }
-    
     team_heroes = {
-        'blue_team': {hero: heroes[hero] for hero in teams_response_data['blue_team']},
-        'red_team': {hero: heroes[hero] for hero in teams_response_data['red_team']}
+        'blue_team': {hero: heroes[hero] for hero in blue_team},
+        'red_team': {hero: heroes[hero] for hero in red_team}
     }
-    
-    print(team_heroes['blue_team'])
-    print(team_heroes['red_team'])
     
     return jsonify(team_heroes)
 
