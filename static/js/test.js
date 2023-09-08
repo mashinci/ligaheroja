6c9f777a96c80b44d0759d89b79a23429ec16400
djolearn <80420514+djolearn@users.noreply.github.com>
2023-09-08 09:18:46 +0200

    wow

--- static/js/test.js
+++ static/js/test.js
@@ -1,11 +1,29 @@
 "use strict";
 // const list = document.querySelector(".list");
+// function show_champs(player) {
+//   var list = document.getElementById(player);
+//   if (list.style.display === "flex") {
+//     list.style.display = "none";
+//   } else {
+//     list.style.display = "flex";
+//     list.style.flexDirection = "column";
+//   }
+// }
 function show_champs(player) {
-  var list = document.getElementById(player);
-  if (list.style.display === "flex") {
-    list.style.display = "none";
-  } else {
+  let list = document.getElementById(player);
+  if (list.style.display === "none" || list.style.display === "") {
     list.style.display = "flex";
     list.style.flexDirection = "column";
+    list.style.opacity = "0";
+    list.style.transition = "opacity 0.3s ease-in";
+    setTimeout(function () {
+      list.style.opacity = "1";
+    }, 10);
+  } else {
+    list.style.opacity = "0";
+    list.style.transition = "opacity 0.3s";
+    setTimeout(function () {
+      list.style.display = "none";
+    }, 300);
   }
 }
