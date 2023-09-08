abdb703f42189e5e63156f3ff1c10a6d7b3bcd4e
djolearn <80420514+djolearn@users.noreply.github.com>
2023-09-08 08:44:49 +0200

    heroes display

--- static/js/test.js
+++ static/js/test.js
@@ -2,9 +2,10 @@
 // const list = document.querySelector(".list");
 function show_champs(player) {
   var list = document.getElementById(player);
-  if (list.style.display === "block") {
+  if (list.style.display === "flex") {
     list.style.display = "none";
   } else {
-    list.style.display = "block";
+    list.style.display = "flex";
+    list.style.flexDirection = "column";
   }
 }
